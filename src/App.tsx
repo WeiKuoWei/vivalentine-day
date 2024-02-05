bb362f13c0f3982501a705d848e3c33b68fa4838
WeiKuoWei <ck3294@nyu.edu>
2024-02-05 13:24:07 +0400

    feat: setup environment

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import "./App.css";
+const phrases = [
+  "No",
+  "Are you sure?",
+  "Really sure?",
+  "Pookie please",
+  "Don't do this to me",
+  "I'm gonna cry...",
+  "You're breaking my heart ;(",
+]
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+    </>
+  );
+}
+
+export default App;
